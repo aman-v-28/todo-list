1589c32b0173cca2163dd5ccf747615c7677594e
aman-v-28 <amanvrajput2001@gmail.com>
2021-08-09 23:24:56 +0530

    initial commit

---
+++ src/app/MyComponents/add-todo/add-todo.component.ts
@@ -0,0 +1,31 @@
+import { Output } from '@angular/core';
+import { EventEmitter } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Todo } from 'src/app/todo';
+
+@Component({
+  selector: 'app-add-todo',
+  templateUrl: './add-todo.component.html',
+  styleUrls: ['./add-todo.component.css']
+})
+export class AddTodoComponent implements OnInit {
+  title!: string;
+  desc!: string;
+  @Output() todoAdd: EventEmitter<Todo> = new EventEmitter();
+  constructor() { }
+
+  ngOnInit(): void {
+    
+  }
+  
+  onSubmit(){
+    const todo = {
+      sno: 8,
+      title: this.title,
+      desc: this.desc,
+      active: true
+    }
+    this.todoAdd.emit(todo);
+  }
+
+}
---
+++ src/app/MyComponents/todo-item/todo-item.component.ts
@@ -0,0 +1,22 @@
+import { Input, Output ,EventEmitter} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Todo } from 'src/app/todo';
+
+@Component({
+  selector: 'app-todo-item',
+  templateUrl: './todo-item.component.html',
+  styleUrls: ['./todo-item.component.css']
+})
+export class TodoItemComponent implements OnInit {
+  @Input() todo!: Todo;
+  @Output() todoDelete: EventEmitter<Todo> = new EventEmitter();
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onclick(todo: Todo){
+    this.todoDelete.emit(todo);
+    console.log("On click has been triggered")
+  }
+}
